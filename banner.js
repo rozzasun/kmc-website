27588243b4fd84b66041fc1f7094b3433678e09f
Rosa Sun <rosa.sun@shopify.com>
2022-01-30 23:40:56 -0500

    Jan 2022 update

--- banner.js
+++ banner.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if (window.location.pathname.split("/").pop() == 'concertDates.html') {
-    setCookie();
-  } else if (!sessionStorage.getItem('viewedUpdate')) {
-    $('#banner').show();
-    $('#banner').on('closed.bs.alert', setCookie);
-    $('#banner > a').on('click', setCookie);
-  } else {
-    $('#banner').hide();
-  }
+  // temporarily disable banner
+  $('#banner').hide();
 
-  function setCookie() {
-    sessionStorage.setItem('viewedUpdate', true);
-  }
+  // if (window.location.pathname.split("/").pop() == 'concertDates.html') {
+  //   setCookie();
+  // } else if (!sessionStorage.getItem('viewedUpdate')) {
+  //   $('#banner').show();
+  //   $('#banner').on('closed.bs.alert', setCookie);
+  //   $('#banner > a').on('click', setCookie);
+  // } else {
+  //   $('#banner').hide();
+  // }
+
+  // function setCookie() {
+  //   sessionStorage.setItem('viewedUpdate', true);
+  // }
 })
